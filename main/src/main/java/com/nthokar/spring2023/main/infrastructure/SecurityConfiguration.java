51fe4a02194915e785509bb53da1d3d590ef4539
nthokar <zhvl03@gmail.com>
2023-10-25 21:01:41 +0500

    spring security fix

---
+++ main/src/main/java/com/nthokar/spring2023/main/infrastructure/SecurityConfiguration.java
@@ -0,0 +1,14 @@
+package com.nthokar.spring2023.main.infrastructure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+class securityConfig {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
+}
